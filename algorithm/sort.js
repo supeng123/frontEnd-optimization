9b1ecb97d46333dc778e4c1e9f9db20e6e03522c
supeng123 <492526873@qq.com>
2019-11-13 21:30:11 +0800

    binary search

--- algorithm/sort.js
+++ algorithm/sort.js
@@ -27,3 +27,28 @@ function findKLargestNumber(array, k) {
 }
 
 console.log(findKLargestNumber([6,7,4,2,1,6,8,9,1,4,3,1,10,11,16,19], 3))
+
+//binary search
+function binarySearch(array, value) {
+    let start = 0;
+    let end = array.length - 1;
+    return binarySearchInside(array, start, end, value)
+}
+
+function binarySearchInside(data, start, end, value) {
+    if (start > end) return -1;
+    let mid = start + ((end - start) >> 1);
+    if (data[mid] === value) {
+        return mid;
+    } else if (data[mid] > value) {
+        end = mid - 1;
+        return binarySearchInside(data, start, end, value);
+    } else {
+        start = mid + 1;
+        return binarySearchInside(data, start, end, value);
+    }
+}
+
+const binaryArray = [1,2,3,6,7,9,99];
+console.log(binarySearch(binaryArray, 3));
+
