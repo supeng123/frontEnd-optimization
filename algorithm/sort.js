0efdd83c90402ca5c970c43a2d5f12db80e73b38
supeng123 <492526873@qq.com>
2019-11-16 21:42:18 +0800

    quicksort

--- algorithm/sort.js
+++ algorithm/sort.js
@@ -88,5 +88,26 @@ const bubbleSortData = [5,3,7,9,1,4]
 bubbleSort(bubbleSortData);
 console.log(bubbleSortData)
 
+//quick sort
+function quickSort(data) {
+    if (data.length <=1) return data;
+    let pivotIndex = Math.floor(data.length / 2);
+    let pivot = data.splice(pivotIndex, 1)[0];
+    let left = [];
+    let right = [];
+    for (var i = 0; i < data.length; i++) {
+        if (data[i] < pivot) {
+            left.push(data[i]);
+        } else {
+            right.push(data[i]);
+        }
+    }
+    return quickSort(left).concat([pivot],quickSort(right));
+}
+
+const quickSortData = [5,3,7,9,1,4]
+const result = quickSort(quickSortData);
+console.log(result)
+
 
 
