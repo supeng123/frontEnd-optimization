2477c02a8f47858d1df9ecd07a7a8ce493b71d9f
supeng123 <492526873@qq.com>
2019-11-19 21:01:48 +0800

    string match

---
+++ algorithm/stringMatch.js
@@ -0,0 +1,25 @@
+function getHashCode(char) {
+    let prime = 1315423911;
+    for (var i = 0; i < char.length; i++) {
+        prime ^= prime << 5 + char.charCodeAt(i) + prime >> 2;
+    }
+    return (prime & 0x7FFFFFFF);
+}
+
+console.log(getHashCode('supeng'))
+
+function findAllPlaces(char, pattern) {
+    const allAppearedIndex = [];
+    const transformedCode = getHashCode(pattern);
+    for (var i = 0; i < char.length - pattern.length; i++) {
+        let currentCode = getHashCode(char.slice(i, i + pattern.length));
+        if (currentCode === transformedCode) {
+            allAppearedIndex.push(i)
+        }
+    }
+    return allAppearedIndex;
+}
+
+const charactors = 'supengsupneg';
+const result = findAllPlaces(charactors, 'su');
+console.log(result)
