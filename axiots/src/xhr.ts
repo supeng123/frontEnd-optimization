b5b69183990b7666587c5f84fa54c1f90d5ac92c
supeng123 <492526873@qq.com>
2020-01-14 19:32:28 +0800

    axios request finish

--- axiots/src/xhr.ts
+++ axiots/src/xhr.ts
@@ -1,7 +1,43 @@
-import {AxiosRequestConfig} from './types/index'
-export default function xhr(config: AxiosRequestConfig) {
-    const {data = null, url, method = 'get'} = config;
-    const request = new XMLHttpRequest();
-    request.open(method.toUpperCase(), url, true);
-    request.send(data);
+import {AxiosRequestConfig, AxiosPromise, AxiosResponse} from './types/index'
+import {parseHeaders} from './helpers/headers'
+
+export default function xhr(config: AxiosRequestConfig):AxiosPromise {
+    return new Promise((resolve) => {
+        const {data = null, url, method = 'get', headers, responseType} = config;
+        const request = new XMLHttpRequest();
+
+        if (responseType) {
+            request.responseType = responseType
+        }
+
+        request.open(method.toUpperCase(), url, true);
+
+        request.onreadystatechange =  function handleChange() {
+            if (request.readyState !== 4) {
+                return 
+            }
+
+            const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+            const responseData = responseType !== 'text' ? request.response : request.responseText
+            const response:AxiosResponse = {
+                data: responseData,
+                status: request.status,
+                statusText: request.statusText,
+                headers: responseHeaders,
+                config,
+                request
+            }
+            resolve(response)
+        }
+
+        Object.keys(headers).forEach((name) => {
+            if (data === null && name.toLowerCase() === 'content-type') {
+                delete headers[name]
+            } else {
+                request.setRequestHeader(name, headers[name])
+            }
+        })
+        request.send(data);
+    })
+    
 }
