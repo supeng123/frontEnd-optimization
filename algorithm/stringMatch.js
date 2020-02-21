1cfec234c7731f56f5e42771c9fe8943a6b64da2
supeng123 <492526873@qq.com>
2020-02-21 22:54:12 +0800

    hash table structure

--- algorithm/stringMatch.js
+++ algorithm/stringMatch.js
@@ -24,4 +24,26 @@ const charactors = 'supengsupneg';
 const result = findAllPlaces(charactors, 'n');
 console.log(result)
 
+//linked address
+// each index will contain an linkedArray or array,if the hash data is repeat, put the repeat data in the linkedArray of array
+//open address
+// if the data is repeat, put it to the avialable space when the empty space can be found
 
+function hasFunc(str, size) {
+    var hashCode = 0
+    for (var i = 0; i < str.length; i++) {
+        hashCode = 37 * hashCode + str.charCodeAt(i)
+    }
+    return hashCode%size
+}
+
+class HashTable {
+    constructor() {
+        this.storage = []
+        //load factor
+        this.count = 0
+        this.limit = 7
+    }
+
+
+}
