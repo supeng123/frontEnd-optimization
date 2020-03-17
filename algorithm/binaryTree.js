e3c81bb0c2e8b12dfd663f7c8d6f3439a67bd1b7
supeng123 <492526873@qq.com>
2020-03-17 21:55:31 +0800

    level order

--- algorithm/binaryTree.js
+++ algorithm/binaryTree.js
@@ -137,6 +137,25 @@ BinaryTree.prototype.delete = function(data) {
 
 }
 
+var levelOrder = function(root) {
+    let levels = []
+    if (!root) return levels;
+    function walk(node, level) {
+        if (levels.length === level) {
+            levels.push([])
+        }
+        levels[level].push(node.value)
+        if (node.left) {
+            walk(node.left, level + 1)
+        }
+        if (node.right) {
+            walk(node.right, level + 1)
+        }
+    }
+    walk(root, 0)
+    return levels
+}
+
 // let tree = new BinaryTree();
 // tree.insert(20);
 // tree.insert(21);
